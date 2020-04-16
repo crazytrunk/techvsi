7ea3c39c6a63240717a447231c03e5a785733619
haihuynh_techvsi <haihuynh@techvsi.com>
2020-04-16 06:33:28 +0000

    backup : 13h30 16/04/20

--- assets/js/home.js
+++ assets/js/home.js
@@ -9,3 +9,48 @@ $(document).ready(function () {
         showCursor: true
     });
 });
+
+$('.vsi-fe1-span-working-hours').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 2000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+  
+    });  
+  
+  });
+
+  $(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
