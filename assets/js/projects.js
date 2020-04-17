7b5cfaf6d03f461ca6fd0cc713a1932b85d4d9bb
HuyHoang15982 <hoang.hdh280396@gmail.com>
2020-04-17 14:18:56 +0700

    feat : Done page Projects BREAKING CHANGE : jquery.zoom.js

---
+++ assets/js/projects.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $('.vsi-fe3-zoom').zoom();
+});
+
+
+$( "#vsi-fe3-btn-more-projects" ).click(function() {
+    $( ".vsi-fe3-projects__projects-list--more-projects" ).addClass('vsi-fe3__more-projects--show');
+    $( ".vsi-fe3-projects__projects-list--more-projects" ).addClass('vsi-fe3__more-projects--animations');
+    $( "#vsi-fe3-btn-more-projects" ).addClass("vsi-btn-more-projects-hide");
+  });
