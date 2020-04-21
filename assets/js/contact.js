ce2898455487b358ddabfd15e877587ef169cbb3
Duy Nghĩa <duynghia120799>
2020-04-21 16:38:33 +0700

    feat ! new code page contact

---
+++ assets/js/contact.js
@@ -0,0 +1,11 @@
+$("#ion_rangeSlider").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 100000,
+    step:5000,
+    from: 25000,
+    to: 50000,
+    prefix: "$"
+});
+
