972ac16f7589cbb2c993fd219c881f2bdf7b4f08
TuyenNguyen <nguyentuyen1322@gmail.com>
2020-04-22 09:49:50 +0700

    backup code backup code lần 1 html css js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,5 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
-$(document).ready(function () {
-    $(".vsi-fe1-hover").hover(
-        function () {
-          $(this).addClass('vsi-fe1-active');
-        }, 
-        function () {
-          $(this).removeClass('vsi-fe1-active');
-        }
-    );
-});
-$(document).ready(function () {
-  $(".vsi-fe1-hover").hover(
-      function () {
-        $(this).addClass('vsi-fe1-active');
-      }, 
-      function () {
-        $(this).removeClass('vsi-fe1-active');
-      }
-  );
-});
+
+
