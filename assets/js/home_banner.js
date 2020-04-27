49c204aebe0fab4508b2b7072949c7cab28b104f
ncongtrung1610 <ncongtrung1610@gmail.com>
2020-04-27 15:18:51 +0700

    backup code banner + what we do

--- assets/js/home_banner.js
+++ assets/js/home_banner.js
@@ -1,3 +1,21 @@
-$( document ).ready(function() {
-    console.log( "banner" );
-});
+var typed = new Typed(".typed-words", {
+    strings: [
+      "Web Development",
+      "Mobile Development",
+      "Email Newsletter (EDM)",
+      "Wordpress",
+      "ReactJS",
+      "AngularJS",
+      "React Native",
+      "PHP ZEND &amp; Magento|"
+    ],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 2000,
+    startDelay: 1000,
+    loop: false,
+    onComplete: function () {
+        $(".typed-cursor").hide();
+    }
+      });
+  
